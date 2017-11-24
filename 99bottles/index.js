d4bde97b72f980825420b47d901165dbbd4334d8
marko delgadillo <markoneseven@gmail.com>
2017-11-24 14:15:25 -0800

    #1 99 bottles of beer song renders completely to the page.

---
+++ 99bottles/index.js
@@ -0,0 +1,27 @@
+let beers = ''
+let verse1 = ' bottles of beer on the wall, '
+let verse2 = ' bottles of beer. Take one down and pass it around, '
+let verse3 = ' bottles of beer on the wall.'
+
+let verse1_2 = ' bottle of beer on the wall, '
+let verse2_2 =
+  ' bottle of beer. Take one down and pass it around, no bottles of beer on the wall.'
+
+const list = document.querySelector('#song')
+
+function bottles() {
+  for (i = 99; i > 0; i--) {
+    beers = i
+    let verse = document.createElement('p')
+    if (beers > 1) {
+      verse.textContent = beers + verse1 + beers + verse2 + (beers - 1) + verse3
+      list.appendChild(verse)
+    } else if (beers === 1) {
+      verse.textContent = beers + verse1_2 + beers + verse2_2
+      list.appendChild(verse)
+    }
+  }
+  return list
+}
+
+bottles()
